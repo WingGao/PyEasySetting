a5dbc584febbcf3b853715cd6cf4e197366db34c
Wing Gao <wing.gao@live.com>
2015-12-15 00:12:29 +0800

    add mongoldb support

--- pyeasysetting.py
+++ pyeasysetting.py
@@ -3,6 +3,9 @@ import json
 import os
 
 SETTING_METHOD_JSON = 'json'
+SETTING_METHOD_MONGO = 'mongodb'
+KEY_NAME = 'key'
+VALUE_NAME = 'value'
 
 
 class PyEasySetting(object):
@@ -37,3 +40,41 @@ class PyEasySettingJSON(PyEasySetting):
     def _save(self):
         with open(self.path, 'w') as f:
             json.dump(self._json, f)
+
+
+class PyEasySettingMongo(PyEasySetting):
+    def __init__(self, host='localhost', port=27017, db='Settings', collection='PyEasySetting'):
+        '''
+            db:database name
+            collection: collection name
+        :param args:
+        :param kwargs:
+        :return:
+        '''
+        import pymongo
+
+        super(PyEasySettingMongo, self).__init__(method=SETTING_METHOD_MONGO)
+
+        self.mongoCon = pymongo.MongoClient(host, port)
+        self.collection = self.mongoCon[db][collection]
+
+    def get(self, key, default=None):
+        v = self.collection.find_one({KEY_NAME: key})
+        if v is None:
+            return default
+        else:
+            return v['value']
+
+    def set(self, key_or_object, value=None, save=True):
+        if isinstance(key_or_object, str):
+            self.collection.update({KEY_NAME: key_or_object}, {KEY_NAME: key_or_object, VALUE_NAME: value}, upsert=True)
+        elif isinstance(key_or_object, dict):
+            for k, v in key_or_object.items():
+                self.set(k, v, save=save)
+
+    def all(self):
+        v = {}
+        for i in self.collection.find({}):
+            v[i[KEY_NAME]] = i[VALUE_NAME]
+
+        return v
--- test.py
+++ test.py
@@ -2,8 +2,14 @@ __author__ = 'wing'
 import pyeasysetting as ezs
 import json
 
+TEST_KEY_NO = 'not_exist'
 TEST_KEY = 'a'
 TEST_VALUE = 'I am a'
+
+TEST_OBJ = {
+    'b': 'Hello b',
+    'c': 'I am c'
+}
 
 
 def res():
@@ -12,6 +18,7 @@ def res():
 
 
 def testJson():
+    print '--- json ---'
     a = ezs.PyEasySettingJSON('test.json')
     print a.get(TEST_KEY)
     a.set(TEST_KEY, TEST_VALUE)
@@ -24,4 +31,15 @@ def testJson():
     res()
 
 
+def testMongo():
+    print '--- mongo ---'
+    a = ezs.PyEasySettingMongo()
+    print a.get(TEST_KEY_NO)
+    a.set(TEST_KEY, TEST_VALUE)
+    print a.get(TEST_KEY)
+    a.set(TEST_OBJ)
+    print a.all()
+
+
 testJson()
+testMongo()
